c0db7ab1040439057fa5a4ea6e5671e0f3bd624d
PatriotRossii <patriotrossii2019@mail.ru>
2021-05-02 18:52:21 +0500

    Add cute test

---
+++ tests/get_members_ids.rs
@@ -0,0 +1,20 @@
+use group_robber::{
+    requests::api_manager::{ApiManager, API_VERSION},
+    stages::groups::Group,
+};
+
+#[tokio::main]
+async fn main() {
+    let mut args = std::env::args();
+
+    let _ = args.next().unwrap();
+    let access_token = args.next().expect("Please, specify argument: ACCESS_TOKEN");
+    let group_id = args.next().expect("Please, specify argument: GROUP_ID");
+
+    let group_id = group_id.parse().expect("Please, specify correct group id");
+
+    let api_manager = ApiManager::new(access_token, API_VERSION);
+    let members = Group::get_members_ids(&api_manager, group_id).await;
+
+    println!("{:#?}", members.unwrap());
+}
